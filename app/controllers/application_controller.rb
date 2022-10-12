eda594e5f98ac5a7cc82f9157d6ab59626301301
austinmei5 <austinmei@uchicago.edu>
2022-10-12 20:15:46 +0000

    started /get_rock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    #write code to do stuff
+
+    # redirect_to("https://wikipedia.org")
+
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
 end
